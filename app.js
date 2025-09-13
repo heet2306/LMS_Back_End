ec9a2847f124071bab681e5dd2eafbdd27bf5380
jyotiteena <jingarjyoti9413@gmail.com>
2025-09-13 15:34:28 +0530

    change cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-require('cors').config()
+require('cors')
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
